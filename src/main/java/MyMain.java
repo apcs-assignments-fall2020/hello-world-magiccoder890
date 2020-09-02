b51d759c04947d18bd8de3cf0c96020fe305c477
magiccoder890 <23fzhu@athenian.org>
2020-09-02 15:03:21 +0000

    homework of Sep 2 Bert

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class MyMain {
     public static void main(String[] args) {
         // Write some code here!
-
+        Scanner scan = new Scanner(System.in);
+		System.out.print("What is your favorite number? ");
+		String name = scan.next();
+        System.out.println("Hello " + name + ". Nice to meet you");
+        Scanner scan2 = new Scanner(System.in);
+        System.out.print("What is your favorite color," + name + "?");
+        String color = scan2.next();
+        System.out.println("Oh,I love " + color + " too");
     }
 }
